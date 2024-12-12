b8e43a79b3af77bd2e9ee8860615b8c7dd2f1ee2
jahanvi kumpavat <jahanvi.kumpavat13@gmail.com>
2024-12-12 22:48:09 +0000

    refactor some code

--- models/bookings/bookingmodel.js
+++ models/bookings/bookingmodel.js
@@ -1,5 +1,6 @@
 const db = require("../../db/connection");
 const { isValidDate } = require("../util");
+const { selectUserBookings } = require("../bookings/select-query");
 
 exports.insertBooking = async (
   { guest_id, check_in_date, check_out_date },
@@ -65,16 +66,7 @@ exports.deleteBooking = async (booking_id) => {
 };
 
 exports.fetchUserBooking = async (guest_id) => {
-  const queryStr = `SELECT booking_id, check_in_date, check_out_date, b.property_id,
-                    p.name AS property_name, 
-                    CONCAT(first_name, ' ', surname) AS host,
-                    (SELECT image_url FROM images i where i.property_id = b.property_id order by image_id limit 1) AS image 
-                    FROM bookings b JOIN properties p
-                    ON b.property_id = p.property_id
-                    JOIN users u
-                    ON b.guest_id = u.user_id 
-                    WHERE guest_id = $1
-                    ORDER BY booking_id`;
+  const queryStr = selectUserBookings;
 
   const { rows: bookings } = await db.query(queryStr, [guest_id]);
   if (bookings.length === 0) {
